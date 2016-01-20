883fba50a23817c37186f26657841249d5162e12
Lucas Reis <lucasmreis@gmail.com>
2016-01-20 21:33:24 -0200

    ignoring boilerplate react native files

--- index.ios.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-import { AppRegistry } from 'react-native';
-
-import RootComponent from './js/components/root-component';
-
-AppRegistry.registerComponent('swipeExp', () => RootComponent);
